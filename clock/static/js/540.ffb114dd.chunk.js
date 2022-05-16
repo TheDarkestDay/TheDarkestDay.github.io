8e5e79b0379f9694cb4bc4782ed92dfeec6819de
TheDarkestDay <alexzund3r@yahoo.com>
2022-05-16 17:29:15 +0000

    Update from https://github.com/TheDarkestDay/xstate-clock/commit/ee9f750872fe38faaf8b30831de7b1e8963f3443

---
+++ clock/static/js/540.ffb114dd.chunk.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var e,t=function e(t){return setTimeout((function(){var t=new Date;postMessage(t),e(60)}),1e3*t)};onmessage=function(n){if("start"===n.data){var a=(new Date).getSeconds();e=t(60-a)}else clearTimeout(e)}}();
+//# sourceMappingURL=540.ffb114dd.chunk.js.map
